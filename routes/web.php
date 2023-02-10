17d00f6aafe99bfc877036a1c637f7f6816a4213
kelvin brown <obasiogbonna9@gmail.com>
2023-02-10 15:31:37 -0700

    updated

--- routes/web.php
+++ routes/web.php
@@ -27,59 +27,19 @@ Route::get('/search', [SearchController::class, 'Search'])->name('search');
 
 
 
-<<<<<<< HEAD
+
 //================ADMIN ALL ROUTES============================================
 Route::middleware(['auth:sanctum', config('jetstream.auth_session')])->group(function () {
     //admin index
-=======
+
 //================ADMIN ALL ROUTES===========================================
->>>>>>> 30ddf2b6d136a52380c5a5821e4ef881118e4e71
+
 Route::get('/home', [AdminController::class, 'Redirect'])->name('home');
 
 //admin logout route
 Route::get('/logout', [AdminController::class, 'Logout'])->name('logout');
 
-<<<<<<< HEAD
-//admin add academics session
-Route::get('/admin/academics', [AdminController::class, 'Academics'])->name('admin-academics');
 
-
-
-});
-//================END ADMIN ALL ROUTES============================================
-
-
-
-
-
-//================PROJECT ALL ROUTES============================================
-Route::middleware(['auth:sanctum', config('jetstream.auth_session')])->group(function () {
-
-//admin view projects
-Route::get('/admin/projects', [ProjectController::class, 'Projects'])->name('admin-view-project');
-
-
-
-});
-
-//================END PROJECT ALL ROUTES============================================
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=======
- 
->>>>>>> 30ddf2b6d136a52380c5a5821e4ef881118e4e71
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
