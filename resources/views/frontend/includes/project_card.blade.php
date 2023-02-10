9123f96f07bb991c107d4a4e6eeca664ff48321e
Myron <josephonyekachi77@gmail.com>
2023-02-10 15:24:38 -0800

    first commit

---
+++ resources/views/frontend/includes/project_card.blade.php
@@ -0,0 +1,62 @@
+<div class="container-xxl py-5">
+        <div class="container">
+            <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+                <h6 class="section-title bg-white text-center text-primary px-3">Projects</h6>
+                <h1 class="mb-5">Latest Projects</h1>
+            </div>
+            <div class="row g-4 justify-content-center">
+                <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3 wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="course-item bg-light">
+                        <div class="text-center p-4 pb-0">
+                            <h3 class="mb-2">Project Title</h3>
+                            
+                            <h5 class="mb-4">Short Description</h5>
+                        </div>
+                        <div class="d-flex border-top">
+                            <small class="flex-fill text-center border-end py-2"><i class="fa fa-user-tie text-primary me-2"></i>John Doe</small>
+                            <small class="flex-fill text-center border-end py-2"><i class="fa fa-clock text-primary me-2"></i>Accademic Year</small>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3 wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="course-item bg-light">
+                        <div class="text-center p-4 pb-0">
+                            <h3 class="mb-2">Project Title</h3>
+                            
+                            <h5 class="mb-4">Short Description</h5>
+                        </div>
+                        <div class="d-flex border-top">
+                            <small class="flex-fill text-center border-end py-2"><i class="fa fa-user-tie text-primary me-2"></i>John Doe</small>
+                            <small class="flex-fill text-center border-end py-2"><i class="fa fa-clock text-primary me-2"></i>Accademic Year</small>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3 wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="course-item bg-light">
+                        <div class="text-center p-4 pb-0">
+                            <h3 class="mb-2">Project Title</h3>
+                            
+                            <h5 class="mb-4">Short Description</h5>
+                        </div>
+                        <div class="d-flex border-top">
+                            <small class="flex-fill text-center border-end py-2"><i class="fa fa-user-tie text-primary me-2"></i>John Doe</small>
+                            <small class="flex-fill text-center border-end py-2"><i class="fa fa-clock text-primary me-2"></i>Accademic Year</small>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3 wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="course-item bg-light">
+                        <div class="text-center p-4 pb-0">
+                            <h3 class="mb-2">Project Title</h3>
+                            
+                            <h5 class="mb-4">Short Description</h5>
+                        </div>
+                        <div class="d-flex border-top">
+                            <small class="flex-fill text-center border-end py-2"><i class="fa fa-user-tie text-primary me-2"></i>John Doe</small>
+                            <small class="flex-fill text-center border-end py-2"><i class="fa fa-clock text-primary me-2"></i>Accademic Year</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
---
+++ resources/views/frontend/includes/search_form.blade.php
@@ -0,0 +1,23 @@
+<div class="container-xxl py-5">
+    <div class="container">
+        <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+            <h6 class="section-title bg-white text-center text-primary px-3">Search</h6>
+            <h1 class="mb-5">Search For Projects</h1>
+        </div>
+        <div class="g-4">
+
+            <form action="">
+                <div class="row g-3">
+                   
+                    <div class="col-12">
+                        <div class="search">
+                            <input type="text" class="form-control" id="subject" placeholder="Search projects...">
+                        </div>
+                    </div>
+                    
+                </div>
+            </form>
+
+        </div>
+    </div>
+</div>
