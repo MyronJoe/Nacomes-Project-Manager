5fa87b3c4f8b875bb0cd33f7f3cc8681339673a4
Myron <josephonyekachi77@gmail.com>
2023-02-11 23:45:56 -0800

    updated

--- resources/views/backend/academics_session/add_session.blade.php
+++ resources/views/backend/academics_session/add_session.blade.php
@@ -8,7 +8,7 @@
         <div class="col-md-12 ">
             <div class="card">
                 <div class="card-header with-border">
-                    <h3 class="card-title ">Manage Projects</h3>
+                    <h3 class="card-title ">Academic Sessions</h3>
                 </div>
                 <div class="card-body">
                     <form class="form" action="{{ route('store-project') }}" method="POST" enctype="multipart/form-data">
@@ -17,62 +17,13 @@
                         <div class="row">
                             <div class="col-md-12 col-12">
                                 <div class="mb-2">
-                                    <label class="form-label mb-1">Project Title</label>
-                                    <input type="text" id="" class="form-control" placeholder="Title" name="title" required />
-                                    @error('title')
-                                    <span class="text-danger">{{ $message }}</span>
-                                    @enderror
+                                    <!-- <label class="form-label mb-1">Academic Session</label> -->
+                                    <input type="text" id="" class="form-control" placeholder="Add academic year" name="year" required />
                                 </div>
                             </div>
 
-                            <div class="col-md-12 col-12">
-                                <div class="mb-2">
-                                    <div class="form-group mb-1">
-                                        <label class="mb-1">Project Year</label>
-                                        <select name="year" class="select2 form-select" id="select2-basic" required>
-                                            <option value="">Select</option>
-                                            <option value="product">Product</option>
-                                            <option value="page">Page</option>
-                                        </select>
-                                        @error('year')
-                                        <span class="text-danger">{{ $message }}</span>
-                                        @enderror
-                                    </div>
-
-                                </div>
-                            </div>
-
-                            <div class="col-md-12 col-12">
-                                <div class="mb-2">
-                                    <label class="form-label mb-1">Student Name</label>
-                                    <input type="text" id="" class="form-control" placeholder="Student" name="student" required />
-                                    @error('student')
-                                    <span class="text-danger">{{ $message }}</span>
-                                    @enderror
-                                </div>
-                            </div>
-                            <div class="col-md-12 col-12">
-                                <div class="mb-2">
-                                    <label class="form-label mb-1">Project Description</label>
-                                    <textarea name="description" id="editor" cols="10" rows="3" class="form-control" required></textarea>
-                                    @error('description')
-                                    <span class="text-danger">{{ $message }}</span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="col-md-12 col-12">
-                                <div class="mb-2">
-                                    <label class="form-label mb-1">Project File</label>
-                                    <input type="file" class="form-control" placeholder="Year" name="project_file" required />
-                                    @error('project_file')
-                                    <span class="text-danger">{{ $message }}</span>
-                                    @enderror
-                                </div>
-                            </div>
                             <div class="card-footer">
-                                <button type="submit" class="btn btn-primary me-1">Submit Project</button>
-                                <button type="reset" class="btn btn-outline-secondary">Reset Form</button>
+                                <button type="submit" class="btn btn-primary me-1">Add Session</button>
                             </div>
                         </div>
                     </form>
