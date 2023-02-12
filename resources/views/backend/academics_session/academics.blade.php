6e9351a9faf568ae772c4d2efa4b963120f59e19
Myron <josephonyekachi77@gmail.com>
2023-02-11 23:30:10 -0800

    updated

--- resources/views/backend/academics_session/academics.blade.php
+++ resources/views/backend/academics_session/academics.blade.php
@@ -1,35 +1,8 @@
 @extends('backend.backend_master')
 @section('content')
 
+<!-- Session Form Start -->
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+<!-- Session Form End -->
 
 @endsection
