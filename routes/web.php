3cb9275f691cd549ec130b680a9b54647d0bacd4
Myron <josephonyekachi77@gmail.com>
2023-02-14 02:34:23 -0800

    updated

--- routes/web.php
+++ routes/web.php
@@ -36,39 +36,36 @@ Route::post('/search-product', [SearchController::class, 'SearchProject'])->name
 Route::middleware(['auth:sanctum', config('jetstream.auth_session')])->group(function () {
     //admin index
 
-//================ADMIN ALL ROUTES===========================================
+    //================ADMIN ALL ROUTES===========================================
 
-Route::get('/home', [AdminController::class, 'Redirect'])->name('home');
+    Route::get('/home', [AdminController::class, 'Redirect'])->name('home');
 
-//admin logout route
-Route::get('/logout', [AdminController::class, 'logout'])->name('logout');
-
-
-//admin Manage academics session
-Route::get('/admin/academics', [AdminController::class, 'Academics'])->name('admin-academics');
+    //admin logout route
+    Route::get('/logout', [AdminController::class, 'logout'])->name('logout');
 
 
-//admin add academics session
-Route::get('/admin/academics/add_session', [AdminController::class, 'Add_session'])->name('add_session');
+    //admin Manage academics session
+    Route::get('/admin/academics', [AdminController::class, 'Academics'])->name('admin-academics');
 
 
-//admin add academics session
-Route::post('/admin/academics/save_session', [AdminController::class, 'Save_session'])->name('save_session');
+    //admin add academics session
+    Route::get('/admin/academics/add_session', [AdminController::class, 'Add_session'])->name('add_session');
 
 
-//admin delete academics session
-Route::get('/admin/academics/delete_session/{id}', [AdminController::class, 'Delete_session'])->name('delete_session');
+    //admin add academics session
+    Route::post('/admin/academics/save_session', [AdminController::class, 'Save_session'])->name('save_session');
 
 
-//admin edit academics session page
-Route::get('/admin/academics/edit_session/{id}', [AdminController::class, 'Edit_session'])->name('edit_session');
+    //admin delete academics session
+    Route::get('/admin/academics/delete_session/{id}', [AdminController::class, 'Delete_session'])->name('delete_session');
 
 
-//admin update academics session in database
-Route::post('/admin/academics/update_session/{id}', [AdminController::class, 'Update_session'])->name('update_session');
+    //admin edit academics session page
+    Route::get('/admin/academics/edit_session/{id}', [AdminController::class, 'Edit_session'])->name('edit_session');
 
 
-
+    //admin update academics session in database
+    Route::post('/admin/academics/update_session/{id}', [AdminController::class, 'Update_session'])->name('update_session');
 });
 //================END ADMIN ALL ROUTES============================================
 
@@ -85,31 +82,37 @@ Route::get('/download/now/{id}', [ProjectController::class, 'DownloadProjects'])
 //================PROJECT ALL ROUTES============================================
 Route::middleware(['auth:sanctum', 'checkregister', config('jetstream.auth_session')])->group(function () {
 
-//admin view projects
-Route::get('/admin/projects', [ProjectController::class, 'Projects'])->name('admin-view-project');
-
-//admin store projects
-Route::post('/admin/project/store', [ProjectController::class, 'AdminStoreProjects'])->name('store-project');
-
-//upload project form
-Route::get('/admin/project/upload', function () {
-
-    $datas = Academics::orderBy('id', 'desc')->get();
-
-    return view('backend.projects.store_project', compact('datas'));
-
-})->name('project-upload');
+    //admin view projects
+    Route::get('/admin/projects', [ProjectController::class, 'Projects'])->name('admin-view-project');
 
 
-//admin delete projet projects
-Route::get('/admin/delete/project/{id}', [ProjectController::class, 'AdminDeleteProjects'])->name('admin-delete-project');
+    //admin store projects
+    Route::post('/admin/project/store', [ProjectController::class, 'AdminStoreProjects'])->name('store-project');
 
-//admin delete projet projects
-Route::get('/admin/edit/project/{id}', [ProjectController::class, 'AdminEditProjects'])->name('edit-project');
+    //upload project form
+    Route::get('/admin/project/upload', function () {
 
-//admin update projet projects
-Route::post('/admin/update/project/{id}', [ProjectController::class, 'AdminUpdateProjects'])->name('update-project');
+        $datas = Academics::orderBy('id', 'desc')->get();
 
+        return view('backend.projects.store_project', compact('datas'));
+    })->name('project-upload');
+
+
+    //admin delete projet projects
+    Route::get('/admin/delete/project/{id}', [ProjectController::class, 'AdminDeleteProjects'])->name('admin-delete-project');
+
+    //admin delete projet projects
+    Route::get('/admin/edit/project/{id}', [ProjectController::class, 'AdminEditProjects'])->name('edit-project');
+
+    //admin update projet projects
+    Route::post('/admin/update/project/{id}', [ProjectController::class, 'AdminUpdateProjects'])->name('update-project');
+
+
+
+    //================ADMIN USERS ALL ROUTES============================================
+
+    //admin view users
+    Route::get('/admin/users', [ProjectController::class, 'Manage_users'])->name('manage_users');
 
 
 });
@@ -138,5 +141,4 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
-    
 });
