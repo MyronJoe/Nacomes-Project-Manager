a8f8fdb1fc315f8619650099533432db5091e763
Myron <josephonyekachi77@gmail.com>
2023-02-16 08:18:27 -0800

    updated

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,7 +1,10 @@
+<!-- HEADER INCLUDE -->
+@include('frontend.includes.header2')
+
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
@@ -32,3 +35,7 @@
         </form>
     </x-jet-authentication-card>
 </x-guest-layout>
+
+
+    <!-- Footer Start -->
+    @include('frontend.includes.footer')
